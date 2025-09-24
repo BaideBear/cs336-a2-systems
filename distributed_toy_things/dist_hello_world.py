ce7ea10163095262b053de0111d7876e28d92b9c
BaideBear <3345180277@qq.com>
2025-09-24 14:03:55 +0800

    add dist demo

---
+++ distributed_toy_things/dist_hello_world.py
@@ -0,0 +1,11 @@
+import os
+import torch
+import torch.distributed as dist
+import torch.multiprocessing as mp
+
+def setup(rank, world_size):
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "29500"
+    dist.init_process_group("gloo", rank=rank, world_size=world_size)
+
+    
